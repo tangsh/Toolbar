42b205ac99daf8a9cf1c8d3756dd02264a934242
513019774@qq.com <513019774@qq.com>
2018-05-11 01:16:41 +0800

    更改 onLayout 增加 elevation 配置

--- demo/src/main/java/com/tsh/toolbar/demo/MainActivity.java
+++ demo/src/main/java/com/tsh/toolbar/demo/MainActivity.java
@@ -1,46 +1,49 @@
 package com.tsh.toolbar.demo;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
+
 import com.tsh.toolbar.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-  Toolbar toolbar3;
-  Toolbar toolbar4;
-  Toolbar toolbar5;
-  Toolbar toolbar6;
+	Toolbar toolbar3;
+	Toolbar toolbar4;
+	Toolbar toolbar5;
+	Toolbar toolbar6;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    toolbar3 = (Toolbar) findViewById(R.id.toolbar3);
+		toolbar3 = (Toolbar) findViewById(R.id.toolbar3);
 //
-    setSupportActionBar(toolbar3);
-    ActionBar actionBar = getSupportActionBar();
-    actionBar.setDisplayHomeAsUpEnabled(true);
+		setSupportActionBar(toolbar3);
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 //
-    toolbar4 = (Toolbar) findViewById(R.id.toolbar4);
-    setSupportActionBar(toolbar4);
-    actionBar = getSupportActionBar();
-    actionBar.setDisplayHomeAsUpEnabled(false);
+		toolbar4 = (Toolbar) findViewById(R.id.toolbar4);
+		setSupportActionBar(toolbar4);
+		actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(false);
 //
-    toolbar5 = (Toolbar) findViewById(R.id.toolbar5);
-    setSupportActionBar(toolbar5);
-    actionBar = getSupportActionBar();
-    actionBar.setDisplayHomeAsUpEnabled(true);
-    //
-    toolbar6 = (Toolbar) findViewById(R.id.toolbar6);
-    setSupportActionBar(toolbar6);
-    actionBar = getSupportActionBar();
-    actionBar.setDisplayHomeAsUpEnabled(true);
-  }
+		toolbar5 = (Toolbar) findViewById(R.id.toolbar5);
+		setSupportActionBar(toolbar5);
+		actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		//
+		toolbar6 = (Toolbar) findViewById(R.id.toolbar6);
+		setSupportActionBar(toolbar6);
+		actionBar = getSupportActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+	}
 
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_main, menu);
+		return true;
+	}
 }
--- toolbarlibrary/src/main/java/com/tsh/toolbar/Toolbar.java
+++ toolbarlibrary/src/main/java/com/tsh/toolbar/Toolbar.java
@@ -2,127 +2,141 @@ package com.tsh.toolbar;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class Toolbar extends android.support.v7.widget.Toolbar {
 
-  private final static String FINAL_TITLE = "##*##";
-  protected TextView mTitleTextView2;
-  protected CharSequence mTitleText2;
+	private final static String FINAL_TITLE = "##*##";
+	protected TextView mTitleTextView2;
+	protected CharSequence mTitleText2;
 
-  private Rect mRect = new Rect();
-  private int[] outLocation = new int[2];
-  private boolean titleCenteredEnable;
+	private boolean titleCenteredEnable;
+	private float mElevation;
+	private View leftView;
 
-  public Toolbar(Context context) {
-    super(context);
-    init(context, null);
-  }
+	public Toolbar(Context context) {
+		super(context);
+		init(context, null);
+	}
 
-  public Toolbar(Context context, @Nullable AttributeSet attrs) {
-    super(context, attrs);
-    init(context, attrs);
-  }
+	public Toolbar(Context context, @Nullable AttributeSet attrs) {
+		super(context, attrs);
+		init(context, attrs);
+	}
 
-  public Toolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-    super(context, attrs, defStyleAttr);
-    init(context, attrs);
-  }
+	public Toolbar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init(context, attrs);
+	}
 
-  private void init(Context context, @Nullable AttributeSet attrs) {
-    TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.Toolbar);
-    titleCenteredEnable = typedArray.getBoolean(R.styleable.Toolbar_titleCentered, false);
-    typedArray.recycle();
-  }
+	private void init(Context context, @Nullable AttributeSet attrs) {
+		TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.Toolbar);
+		titleCenteredEnable = typedArray.getBoolean(R.styleable.Toolbar_titleCentered, false);
+		mElevation = typedArray.getDimension(R.styleable.Toolbar_elevation, 5);
+		typedArray.recycle();
+		ViewCompat.setElevation(this, mElevation);
+	}
 
-  @Override public void setTitle(CharSequence title) {
+	@Override
+	public void setTitle(CharSequence title) {
 
-    this.mTitleText2 = title;
+		this.mTitleText2 = title;
 
-    if (mTitleTextView2 == null) {
-      super.setTitle(FINAL_TITLE);
-      // find title view
-      mTitleTextView2 = findTitleView();
-    }
-    //
-    if (mTitleTextView2 != null) {
-      mTitleTextView2.setTranslationX(0);
-      mTitleTextView2.setText(title);
-    }
-  }
+		if (mTitleTextView2 == null) {
+			super.setTitle(FINAL_TITLE);
+			// find title view
+			mTitleTextView2 = findTitleView();
+		}
+		//
+		if (mTitleTextView2 != null) {
+			mTitleTextView2.setText(title);
+		}
+	}
 
-  private TextView findTitleView() {
-    View childView;
-    TextView titleView = null, textView;
-    for (int i = 0, count = getChildCount(); i < count; i++) {
-      childView = getChildAt(i);
-      if (childView instanceof TextView) {
-        textView = (TextView) childView;
-        if (FINAL_TITLE.equals(textView.getText().toString())) {
-          titleView = textView;
-        }
-      }
-    }
-    return titleView;
-  }
+	private TextView findTitleView() {
+		View childView;
+		TextView titleView = null, textView;
+		for (int i = 0, count = getChildCount(); i < count; i++) {
+			childView = getChildAt(i);
+			if (childView instanceof TextView) {
+				textView = (TextView) childView;
+				if (FINAL_TITLE.equals(textView.getText().toString())) {
+					titleView = textView;
+				}
+			}
+		}
+		return titleView;
+	}
 
-  @Override protected void onDraw(Canvas canvas) {
-    if (titleCenteredEnable) {
-      adjustTitlePosition();
-    }
-    super.onDraw(canvas);
-  }
+	@Override
+	protected Parcelable onSaveInstanceState() {
+		Bundle bundle = new Bundle();
+		Parcelable parcelable = super.onSaveInstanceState();
+		bundle.putParcelable("com_tsh_toolbar_super_data", parcelable);
+		bundle.putBoolean("com_tsh_toolbar_state_title_centered_enable", titleCenteredEnable);
+		bundle.putDouble("com_tsh_toolbar_state_elevation", mElevation);
+		return bundle;
+	}
 
-  /**
-   * 调整标题居中
-   */
-  private void adjustTitlePosition() {
-    if (mTitleTextView2 == null) return;
+	@Override
+	protected void onRestoreInstanceState(Parcelable state) {
+		if (state != null) {
+			Bundle bundle = (Bundle) state;
+			Parcelable superData = bundle.getParcelable("com_tsh_toolbar_super_data");
+			titleCenteredEnable = bundle.getBoolean("com_tsh_toolbar_state_title_centered_enable", false);
+			mElevation = (float) bundle.getDouble("com_tsh_toolbar_state_elevation", mElevation);
+			ViewCompat.setElevation(this, mElevation);
+			super.onRestoreInstanceState(superData);
+		} else {
+			super.onRestoreInstanceState(state);
+		}
+	}
 
-    mTitleTextView2.getLocalVisibleRect(mRect);
-    if (mRect == null || mRect.width() == 0) return;
+	public TextView getTitleTextView() {
+		return mTitleTextView2;
+	}
 
-    String text = mTitleTextView2.getText().toString();
-    if (mTitleText2 != null) {
-      text = mTitleText2.toString();
-    }
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
-    float textWidth = mTitleTextView2.getPaint().measureText(text);
+		super.onLayout(changed, l, t, r, b);
 
-    if (textWidth <= mRect.width() && mTitleTextView2.getTranslationX() == 0) {
+		final boolean isRtl = ViewCompat.getLayoutDirection(this) == ViewCompat.LAYOUT_DIRECTION_RTL;
 
-      mTitleTextView2.getLocationInWindow(outLocation);
-      int left = outLocation[0];
+		if (titleCenteredEnable && shouldLayoutView(mTitleTextView2)) {
+			int val;
+			if (isRtl) {
+				val = (getMeasuredWidth() + mTitleTextView2.getMeasuredWidth()) / 2;
+				mTitleTextView2.layout(val - mTitleTextView2.getMeasuredWidth(), mTitleTextView2.getTop(),
+						val, mTitleTextView2.getBottom());
+			} else {
+				val = (getMeasuredWidth() - mTitleTextView2.getMeasuredWidth()) / 2;
+				mTitleTextView2.layout(val, mTitleTextView2.getTop(),
+						mTitleTextView2.getMeasuredWidth() + val, mTitleTextView2.getBottom());
+			}
+		}
 
-      int translationX = (int) ((getMeasuredWidth() - textWidth) / 2 - left);
+		if (leftView != null) {
+			leftView.layout(0, leftView.getTop(), leftView.getRight() - leftView.getLeft(),
+					leftView.getBottom());
+			leftView.setVisibility(VISIBLE);
+		}
+	}
 
-      mTitleTextView2.setTranslationX(translationX);
-    }
-  }
+	@Override
+	protected void onFinishInflate() {
+		super.onFinishInflate();
+		leftView = findViewById(R.id.Toolbar_leftViewId);
+	}
 
-  @Override protected Parcelable onSaveInstanceState() {
-    Bundle bundle = new Bundle();
-    Parcelable parcelable = super.onSaveInstanceState();
-    bundle.putParcelable("com_tsh_toolbar_super_data", parcelable);
-    bundle.putBoolean("com_tsh_toolbar_state_title_centered_enable", titleCenteredEnable);
-    return bundle;
-  }
-
-  @Override protected void onRestoreInstanceState(Parcelable state) {
-    if (state != null) {
-      Bundle bundle = (Bundle) state;
-      Parcelable superData = bundle.getParcelable("com_tsh_toolbar_super_data");
-      titleCenteredEnable = bundle.getBoolean("com_tsh_toolbar_state_title_centered_enable", false);
-      super.onRestoreInstanceState(superData);
-    } else {
-      super.onRestoreInstanceState(state);
-    }
-  }
+	protected boolean shouldLayoutView(View view) {
+		return view != null && view.getParent() == this && view.getVisibility() != GONE;
+	}
 }
